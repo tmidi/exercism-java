cd9dfe1c2403c63f8cfcc5bc5984c7867cd0b796
tmidi <taleebmidi@gmail.com>
2018-05-06 23:47:44 -0400

    Solution #1 for RNA Transcription

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,22 @@
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i <= dnaStrand.length()-1; i++ ) {
+            if (dnaStrand.charAt(i) == 'G') {
+                result.append('C');
+            } else if (dnaStrand.charAt(i) == 'C') {
+                result.append('G');
+            } else if (dnaStrand.charAt(i) == 'T') {
+                result.append('A');
+            } else if (dnaStrand.charAt(i) == 'A') {
+                result.append('U');
+            } else {
+                throw new IllegalArgumentException();
+            }
+
+        }
+        return result.toString();
     }
 
 }
